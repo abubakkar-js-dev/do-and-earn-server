6a3e04b0764acf2fbc7497f98c6447eb9935a07c
abubakkar-js-dev <mdabubakkars182@gmail.com>
2025-01-24 08:05:36 +0600

    update: commented ping related code

--- index.js
+++ index.js
@@ -33,10 +33,10 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
 
     const tasksCollection = client.db("doAndearn").collection("tasks");
     const usersCollection = client.db("doAndearn").collection("users");
